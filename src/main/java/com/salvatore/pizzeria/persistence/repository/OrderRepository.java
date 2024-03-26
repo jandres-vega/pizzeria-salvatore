adfe7492768f5d620a05e73cfcf45a3b818eaf23
jandres-vega <jandresvega@itsa.edu.co>
2024-03-26 17:59:34 -0500

    Add repositorys

---
+++ src/main/java/com/salvatore/pizzeria/persistence/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.salvatore.pizzeria.persistence.repository;
+
+import com.salvatore.pizzeria.persistence.entity.OrderEntity;
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends ListCrudRepository<OrderEntity, Integer> {
+
+    List<OrderEntity> findAllByDateAfter(LocalDateTime date);
+
+    List<OrderEntity> findAllByMethodIn(List<String> methods);
+
+}
